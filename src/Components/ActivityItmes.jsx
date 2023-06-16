1b40cd7c2021a12768a9194ef707a40e95bbabdb
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-06-16 14:58:14 +0500

    edit activity section

--- src/Components/ActivityItmes.jsx
+++ src/Components/ActivityItmes.jsx
@@ -8,32 +8,32 @@ import Img6 from "../assets/activity-icons/6.png";
 export const ActivityItems = [
   {
     icon: Img,
-    title: "IT news from around the world",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet tenetur facere nulla voluptates laudantium ullam delectus assumenda, ab obcaecati.",
+    title: "IT news In The World",
+    desc: "We share not only Karakalpakstan but foreign IT news. You can know all the IT news from around the world through our website. News about IT in the world is very interesting for you. You can further expand your worldview through foreign IT news.",
   },
   {
     icon: Img2,
     title: "Share more updates",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet tenetur facere nulla voluptates laudantium ullam delectus assumenda, ab obcaecati.",
+    desc: "We share IT news faster and more efficiently. You can get a lot of information through our little website. Join us and be the first to know about IT news.",
   },
   {
     icon: Img3,
     title: "Daily list of news",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet tenetur facere nulla voluptates laudantium ullam delectus assumenda, ab obcaecati.",
+    desc: "We view, check, and list daily IT news. All our news is reliable. We share reliable information with you through the website Join us and follow the reliable information about IT.",
   },
   {
     icon: Img4,
     title: "Check the news",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet tenetur facere nulla voluptates laudantium ullam delectus assumenda, ab obcaecati.",
+    desc: "We have all the IT news that you cannot find. Be sure to visit the site after Google and get acquainted with IT news from us. You can find the latest news with us without searching. Invite your friends to visit the site.",
   },
   {
     icon: Img5,
     title: "Reliable news",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet tenetur facere nulla voluptates laudantium ullam delectus assumenda, ab obcaecati.",
+    desc: "We definitely like all news reliable fake news. All you need to view these updates is the internet and any device you like, such as a computer, notebook, tablet or smartphone, and you can use the website as you like.",
   },
   {
     icon: Img6,
     title: "Online in 24 hours",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eveniet tenetur facere nulla voluptates laudantium ullam delectus assumenda, ab obcaecati.",
+    desc: "We are online 24 hours a day, so you can access the website anytime you want. You can connect to us at any time through the website. News on the website is shared for 24 hours and after 24 hours it will be revealed as outdated news.",
   },
 ];
