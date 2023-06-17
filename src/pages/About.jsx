95dbd0db0057f224462428627116167e2d28d2f5
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-06-17 23:00:30 +0500

    create about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import Img from "../assets/about-img.png";
+import "../styles/About.scss";
 
 const About = () => {
   return (
-    <div>
-      This is a About page!
+    <div className="about">
+      <div className="text-content">
+        <span>About Us</span>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          tempore voluptate adipisci illum quidem magni veritatis reprehenderit
+          iste eius, nihil est praesentium ducimus et quos, placeat repudiandae
+          enim dolor animi?
+        </p>
+        <button className="btn">See more</button>
+      </div>
+      <img src={Img} alt="" />
     </div>
   );
 };
