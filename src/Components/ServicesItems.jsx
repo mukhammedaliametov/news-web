1d156266a3b0b34be9af0d540de5aff81abaf47b
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-06-18 20:41:08 +0500

    create services page and style

---
+++ src/Components/ServicesItems.jsx
@@ -0,0 +1,33 @@
+export const ServicesItems = [
+  {
+    icon: 'fa-regular fa-newspaper',
+    title: 'Share IT news',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus similique esse modi laudantium sit. Mollitia nihil cum tenetur quisquam!'
+  },
+  {
+    icon: 'fa-regular fa-newspaper',
+    title: 'Share IT news',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus similique esse modi laudantium sit. Mollitia nihil cum tenetur quisquam!'
+  },
+  {
+    icon: 'fa-regular fa-newspaper',
+    title: 'Share IT news',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus similique esse modi laudantium sit. Mollitia nihil cum tenetur quisquam!'
+  },
+  {
+    icon: 'fa-regular fa-newspaper',
+    title: 'Share IT news',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus similique esse modi laudantium sit. Mollitia nihil cum tenetur quisquam!'
+  },
+  {
+    icon: 'fa-regular fa-newspaper',
+    title: 'Share IT news',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus similique esse modi laudantium sit. Mollitia nihil cum tenetur quisquam!'
+  },
+  {
+    icon: 'fa-regular fa-newspaper',
+    title: 'Share IT news',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus similique esse modi laudantium sit. Mollitia nihil cum tenetur quisquam!'
+  },
+]
+
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { ServicesItems } from '../Components/ServicesItems';
+import '../styles/Services.scss';
 
 const Services = () => {
   return (
-    <div>
-      This is a Services page!
+    <div className='services'>
+      <h1>Services</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, minus!</p>
+      <div className="cards">
+        {ServicesItems.map((item, index) => {
+          return(
+            <div className="card" key={index}>
+              <i className={item.icon}></i>
+              <h1>{item.title}</h1>
+              <span>{item.desc}</span>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 };
